b0081b70754e25bd8c6e021868b66b7bf99da7cf
lijunlong <lijunlong@lijunlong.local>
2012-10-16 11:02:22 +0800

    Initial Commit

---
+++ UITabBar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITabBar
+//
+//  Created by lijunlong on 12-10-16.
+//  Copyright (c) 2012年 lijunlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
